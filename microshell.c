afb349755cddd7aaf23fe308e25568092a78b1e2
vcart <vcart@student.42lyon.fr>
2023-04-26 21:05:50 +0200

    fixed cd

--- microshell.c
+++ microshell.c
@@ -53,7 +53,7 @@ int	main(int argc, char **argv, char **envp)
 		{
 			if (i != 2)
 				ft_putstr_error("error : cd : bad arguments", NULL);
-			else if (chdir(argv[i]) != 0)
+			else if (chdir(argv[1]) != 0)
 				ft_putstr_error("error : cd : cannot change directory to ", argv[1]);
 		}
 		else if (i != 0 && (argv[i] == NULL || strcmp(argv[i], ";") == 0))
